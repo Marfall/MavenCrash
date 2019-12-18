915f8542008ac26b3c28ba89a00b6d72bb27e7b3
Marfall <marinefalling@gmail.com>
2019-12-18 20:26:15 +0300

    dependency begin

--- MvnProject/src/main/java/Main.java
+++ MvnProject/src/main/java/Main.java
@@ -1,5 +1,9 @@
+import org.apache.log4j.Logger;
+
 public class Main {
+    Logger logger = Logger.getLogger(Main.class);
     public static void main(String[] args) {
+
         System.out.println("Fuck this shit, I'm going to Narnia");
 
     }
